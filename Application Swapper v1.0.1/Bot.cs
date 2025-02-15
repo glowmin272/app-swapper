e38efec9478b490c7fa216351318376400d4a012
glowmin272 <glowmin272@gmail.com>
2025-02-15 14:37:26 -0500

    Add files via upload
    Restructured the entire bot for somewhat improved flow.
    ? commands are somewhat broken, but there is error handling to make sure the bot doesn't go down.

---
+++ Application Swapper v1.0.1/Bot.cs
@@ -0,0 +1,49 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using System;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application_Swapper
+{
+    public class Bot
+    {
+        private readonly DiscordSocketClient _client;
+        private readonly CommandHandler _commandHandler;
+
+        public Bot()
+        {
+            _client = new DiscordSocketClient(new DiscordSocketConfig
+            {
+                GatewayIntents = GatewayIntents.MessageContent | GatewayIntents.Guilds | GatewayIntents.GuildMessages
+            });
+
+            _commandHandler = new CommandHandler(_client);
+        }
+
+        public async Task RunAsync()
+        {
+            _client.Log += LogAsync;
+            _client.Ready += ReadyAsync;
+
+            await _client.LoginAsync(TokenType.Bot, "token");
+            await _client.StartAsync();
+            await _commandHandler.InitializeAsync();
+            await Task.Delay(-1);
+        }
+
+        private async Task ReadyAsync()
+        {
+            Console.WriteLine("Bot is online!");
+            await _commandHandler.RegisterSlashCommandsAsync();
+        }
+
+        private Task LogAsync(LogMessage log)
+        {
+            Console.WriteLine(log);
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Application Swapper v1.0.1/DSlashCommands.cs
@@ -0,0 +1,22 @@
+﻿using Discord.WebSocket;
+
+public class FakeSlashCommand
+{
+    public SocketUser User { get; }
+    public string Character { get; }
+
+    public FakeSlashCommand(SocketUser user, string character)
+    {
+        User = user;
+        Character = character;
+    }
+
+    public async Task RespondAsync(string response)
+    {
+        if (User is SocketUser socketUser)
+        {
+            var dmChannel = await socketUser.CreateDMChannelAsync();
+            await dmChannel.SendMessageAsync(response);
+        }
+    }
+}
